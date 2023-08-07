7a1decb2c28f4242a04f9563444bfc5cf1420e48
AMJADALMOHTASEB <amjad.almohtaseb@rhinosoft.io>
2023-08-07 13:51:58 +0300

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+document.addEventListener('click', function (e) {
+  if (e.target.className === 'show-modal') {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  } else if (
+    (!modal.contains(event.target) || e.target.className === 'close-modal') &&
+    !modal.classList.contains('hidden')
+  ) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
